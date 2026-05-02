ae31620f54d9c3c5df57a9a2b0da52b8893433b7
IKT <kit311290@gmail.com>
2026-05-02 17:33:53 +0300

    done mutation

--- Assets/_Project/_Scripts/Gameplay/Units/Unit.cs
+++ Assets/_Project/_Scripts/Gameplay/Units/Unit.cs
@@ -1,35 +1,105 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using Zenject;
 
 public class Unit : MonoBehaviour
 {
-  
+
     [SerializeField] private UnitMove _unitMove;
     [SerializeField] private UnitHealth _unitHealth;
     [SerializeField] private UnitView _unitView;
 
     private UnitsFactory factory;
+    private UnitConfig _config;
+    private IConfigProvider _configProvider;
+    private Coroutine _lifePhaseRoutine;
 
-	public UnitHealth Damageble => _unitHealth;
+    public event Action Mutated;
+
+    public UnitHealth Damageble => _unitHealth;
 
     public UnitType UnitType { get; private set; }
+
+    [Inject]
+    public void Construct(IConfigProvider configProvider) =>
+       _configProvider = configProvider;
+
     public void SetPool(UnitsFactory factory) => this.factory = factory;
+
     private void OnSpawned(UnitConfig config)
     {
-        UnitType = config.UnitType;
-
-       _unitView.Init(config.UnitType);
-        _unitMove.Init(config.StartMoveSpeed);
-        _unitHealth.Init(config.StartHealth, config.MainDamagePercent, config.SecondaryDamagePercent);
+        _unitHealth.ZeroHealth -= OnZeroHealth;
+        ApplyConfig(config);
         _unitHealth.ZeroHealth += OnZeroHealth;
 
+        StartLifePhase();
+    }
+
+     private void StartLifePhase()
+    {
+        StopLifePhase();
+        _lifePhaseRoutine = StartCoroutine(LifePhaseRoutine());
+    }
+
+     private IEnumerator LifePhaseRoutine()
+    {
+        while (_config != null)
+        {
+            UnitConfig currentConfig = _config;
+
+            if (currentConfig.MinStayTime <= 0)
+            {
+                Debug.LogWarning("[Unit] Min stay time should be greater than zero");
+                yield break;
+            }
+
+            yield return new WaitForSeconds(currentConfig.MinStayTime);
+
+            UnitConfig mutationUnitConfig = _configProvider.GetRandomUnitMutationConfig();
+
+            if (mutationUnitConfig == null || mutationUnitConfig == _config)
+                continue;
+
+            Mutation(mutationUnitConfig);
+        }
+    }
+
+     private void Mutation(UnitConfig config)
+    {
+        ApplyConfig(config);
+        Mutated?.Invoke();
+    }
+
+    private void StopLifePhase()
+    {
+        if (_lifePhaseRoutine == null)
+            return;
+
+        StopCoroutine(_lifePhaseRoutine);
+        _lifePhaseRoutine = null;
+    }
+
+
+
+    private void ApplyConfig(UnitConfig config)
+    {
+        if (config == null)
+            return;
+
+        _config = config;
+        UnitType = config.UnitType;
+
+        _unitView.Init(config.UnitType);
+        _unitMove.Init(config.StartMoveSpeed);
+        _unitHealth.Init(config.StartHealth, config.MainDamagePercent, config.SecondaryDamagePercent);
     }
 
     private void OnZeroHealth()
     {
         Debug.Log("Gnome is dead");
 
-		factory.DespawnUnit(this);
+        factory.DespawnUnit(this);
     }
 
     private void OnDespawned()
