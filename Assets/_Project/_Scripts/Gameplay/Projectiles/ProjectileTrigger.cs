7780a65b1d32e9b0a2ca258918067aadc3d0cb3d
IKT <kit311290@gmail.com>
2026-05-03 12:03:14 +0300

    fix bug: The projectile can be dragged around and used to kill units

--- Assets/_Project/_Scripts/Gameplay/Projectiles/ProjectileTrigger.cs
+++ Assets/_Project/_Scripts/Gameplay/Projectiles/ProjectileTrigger.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D), typeof(Rigidbody2D))]
 public class ProjectileTrigger : MonoBehaviour
 {
+    [SerializeField] private Shooting _shooting;
+
     private ProjectileType _projectileType;
 
     public void Init(ProjectileConfig projectileConfig)
@@ -14,12 +16,15 @@ public class ProjectileTrigger : MonoBehaviour
     {
         var collider = GetComponent<Collider2D>();
         collider.isTrigger = true;
+
+        if(_shooting == null)
+        _shooting = GetComponent<Shooting>();
     }
 
     void OnTriggerEnter2D(Collider2D collision)// temp/ todo -> if unit in collider at the moment of explosion
     {
 
-        if (collision.TryGetComponent<Unit>(out var unit))
+        if (collision.TryGetComponent<Unit>(out var unit)  && _shooting.IsMoving)
         {
             if (CompatibilityExtention.IsMainDamage(unit.UnitType, _projectileType))
                 unit.Damageble.DealMainDamage();
